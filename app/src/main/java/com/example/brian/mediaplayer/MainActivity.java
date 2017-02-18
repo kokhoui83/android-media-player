632776fe4b939301d2cb2bfbc4bb9f158fa9ebd2
brian.lim <brianlim@iflix.com>
2017-02-18 23:52:37 +0800

    Refactor code

--- app/src/main/java/com/example/brian/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/brian/mediaplayer/MainActivity.java
@@ -2,15 +2,12 @@ package com.example.brian.mediaplayer;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
@@ -28,33 +25,22 @@ import com.google.android.exoplayer2.util.Util;
 public class MainActivity extends AppCompatActivity {
     SimpleExoPlayerView playerView;
     SimpleExoPlayer player;
-    Handler mainHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initPlayer();
-
         Uri mp4VideoUri = Uri.parse("http://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd");
 
-        // Measures bandwidth during playback. Can be null if not required.
-        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        // Produces DataSource instances through which media data is loaded.
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "MediaPlayer"), bandwidthMeter);
-        // Produces Extractor instances for parsing the media data.
-        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-        // This is the MediaSource representing the media to be played.
-        MediaSource videoSource = new DashMediaSource(mp4VideoUri, dataSourceFactory, new DefaultDashChunkSource.Factory(dataSourceFactory), null, null);
-        // Prepare the player with the source.
-        player.prepare(videoSource);
+        initPlayer();
+        preparePlayer(mp4VideoUri);
+
 
     }
 
     private void initPlayer() {
         // Create default TrackSelector
-        mainHandler = new Handler();
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
         TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
         TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
@@ -67,5 +53,16 @@ public class MainActivity extends AppCompatActivity {
 
         playerView = (SimpleExoPlayerView) findViewById(R.id.player);
         playerView.setPlayer(player);
+    }
+
+    private void preparePlayer(Uri mp4VideoUri) {
+        // Measures bandwidth during playback. Can be null if not required.
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        // Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "MediaPlayer"), bandwidthMeter);
+        // This is the MediaSource representing the media to be played.
+        MediaSource videoSource = new DashMediaSource(mp4VideoUri, dataSourceFactory, new DefaultDashChunkSource.Factory(dataSourceFactory), null, null);
+        // Prepare the player with the source.
+        player.prepare(videoSource);
     }
 }
